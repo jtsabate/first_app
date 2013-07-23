ee76a1639b156a46d758f0efb514d36fb2f04dba
jtsabate <jts@jts-VirtualBox.(none)>
2013-07-22 20:11:45 -0400

    Inicializando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '2c4c91361878cbdbe3f16b666114c2f08aef6bc5b6b1bd219c54c7b2e11457bc5f1079bd0c01adf3eea8f85d8c288683bcdc2e82c4304b05d7b728f81f807a3a'
